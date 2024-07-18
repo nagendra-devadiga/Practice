808e3c9263936f795284d5f13c166fc6346ff373
Nagendra Devadiga(nkpd) <{ID}+{username}@users.noreply.github.com>
2024-07-18 19:48:35 +0530

    Added optimized Euclid algorithm

--- src/Numbers/GCD.java
+++ src/Numbers/GCD.java
@@ -15,7 +15,8 @@ public class GCD {
 
 //        int gcd = gcdBrutForce(a,b);
 
-            int gcd = gcdEuclid(a, b);
+//            int gcd = gcdEuclid(a, b);
+            int gcd = gcdOptimizedEuclid(a, b);
             System.out.println("gdc is: "+ gcd);
             System.out.println("Do you want to continue?");
             continuing = yesOrNo.next();
@@ -30,15 +31,6 @@ public class GCD {
      * 3. repeat the steps until the both number equal (i.e difference is 0)
      */
     private static int gcdEuclid(int a, int b) {
-//        if(a == b)
-//            return a;
-//        int max = Math.max(a, b);
-//        int min = Math.min(a, b);
-//        return gcdEuclid(max-min, min);
-
-        /**
-         * using while loop
-         */
         while (a!=b){
             if(a>b)
                 a = a-b;
@@ -46,6 +38,22 @@ public class GCD {
                 b = b-a;
         }
         return a;
+    }
+
+    /**
+     *  optimized Euclid's algorithm
+     * 1. find the larger number
+     * 2. replace the larger number by the modulus
+     * 3. repeat the steps until any of the number becomes 0
+     */
+    private static int gcdOptimizedEuclid(int a, int b) {
+        while (a!=0 && b!=0){
+            if(a>b)
+                a = a%b;
+            else
+                b = b%a;
+        }
+        return a == 0 ? b : a;
     }
 
     private static int gcdBrutForce(int a, int b) {
